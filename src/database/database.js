c3f4a1b9bd212d7f4813903f6dc30da2d595c001
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-06-18 16:24:44 -0500

    🚧 add sequelize schemas

--- src/database/database.js
+++ src/database/database.js
@@ -4,9 +4,9 @@ import { config } from "../config/index.js";
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+console.log(URI)
 
 export const sequelize = new Sequelize(URI, {
-  host: "postgres",
   dialect: "postgres",
   logging: console.log,
 });
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
+import "./models/Project.js";
 
 async function main() {
   try {
-    await sequelize.authenticate();
+    console.log('Attempting database synchronization');
+    await sequelize.sync({ force: false });
     console.log("Connection has been established successfully.");
 
     app.listen(3000);
-    console.log("===================================")
+    console.log("===================================");
     console.log("Server is running on port 3000");
-    console.log("===================================")
+    console.log("===================================");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
 }
+
+main();

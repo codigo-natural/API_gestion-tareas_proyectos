444da4ea00601b1b8f44c126381eaa41cef8a9fb
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-06-19 04:43:41 -0500

    🚧 Tasks - create and read

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
-import projectRoutes from "./routes/projects.routes.js"
+import projectRoutes from "./routes/projects.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
 
-const app = express()
+const app = express();
 
 // middlewares
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api', projectRoutes)
+app.use("/api", projectRoutes);
+app.use("/api", tasksRoutes);
 
 export default app;

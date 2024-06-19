fc2d3c1786e66f48ecc019edfc1f5ff49205055d
Ivan Camilo <buitragoivancamilo1025@gmail.com>
2024-06-19 05:22:39 -0500

    🚧 add relations

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
-import { createProject, deleteProject, getProject, getProjects, updateProject } from "../controllers/project.controller.js";
+import {
+  createProject,
+  deleteProject,
+  getProject,
+  getProjects,
+  updateProject,
+  getProjectTasks,
+} from "../controllers/project.controller.js";
 
 const router = Router();
 
-router.get(('/projects'), getProjects)
-router.post(('/projects'), createProject)
-router.put(('/projects/:id'), updateProject)
-router.delete(('/projects/:id'), deleteProject)
-router.get(('/projects/:id'), getProject)
+router.get("/projects", getProjects);
+router.post("/projects", createProject);
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
+router.get("/projects/:id", getProject);
+
+router.get("/projects/:id/tasks", getProjectTasks);
 
 export default router;
